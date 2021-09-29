8337ca70b7733ab3a4cad7e00166a4f16cfa4514
LucasFlaquer <lucas.flaquer@gmail.com>
2021-09-29 14:11:52 -0300

    eslint configured

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,14 @@
-import express from 'express';
+import express from "express";
 
 const app = express();
 
 app.get("/", (request, response) => {
-  return response.json({message: 'Hello world'})
-})
+  return response.json({ message: "Hello world" });
+});
 
-app.listen(3333, () => console.log('Server is Running!'));
+app.post("/courses", (request, response) => {
+  const { name } = request.body;
+  return response.json({ name });
+});
+
+app.listen(3333, () => console.log("Server is Running!"));

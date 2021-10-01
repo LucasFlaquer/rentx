795548c524110a728efb7ec883f92168891b9840
LucasFlaquer <lucas.flaquer@gmail.com>
2021-10-01 16:40:57 -0300

    Dependency inversion and SRP

--- src/routes/categories.routes.ts
+++ src/routes/categories.routes.ts
@@ -1,17 +1,19 @@
 import { Router } from "express";
 
 import { CategoriesRepository } from "../repositories/CategoriesRepository";
+import { CreateCategoryService } from "../services/CreateCategoryService";
 
 const categoriesRoutes = Router();
 const categoriesRepository = new CategoriesRepository();
 
 categoriesRoutes.post("/", (request, response) => {
   const { name, description } = request.body;
-  const categoryAlreadyExists = categoriesRepository.findByName(name);
-  if (categoryAlreadyExists)
-    return response.status(400).json({ error: "category already exists!" });
-
-  categoriesRepository.create({ name, description });
+  const createCategoryService = new CreateCategoryService(categoriesRepository);
+  try {
+    createCategoryService.execute({ name, description });
+  } catch (error) {
+    return response.status(400).json({ error: error.message });
+  }
   return response.status(201).send();
 });
 categoriesRoutes.get("/", (request, response) => {

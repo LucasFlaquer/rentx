d59a4b0c738ab68f21757dd65d740a45b2014ca0
LucasFlaquer <lucas.flaquer@gmail.com>
2021-10-01 16:15:44 -0300

    categorias

---
+++ src/repositories/CategoriesRepository.ts
@@ -0,0 +1,28 @@
+import { Category } from "../model/Category";
+
+interface ICreateCategoryDTO {
+  name: string;
+  description: string;
+}
+
+export class CategoriesRepository {
+  private categories: Category[];
+
+  constructor() {
+    this.categories = [];
+  }
+
+  public create({ name, description }: ICreateCategoryDTO): void {
+    const category = new Category();
+    Object.assign(category, { name, description, created_at: new Date() });
+    this.categories.push(category);
+  }
+  public list(): Category[] {
+    return this.categories;
+  }
+
+  public findByName(name: string): Category {
+    const category = this.categories.find((category) => category.name === name);
+    return category;
+  }
+}
